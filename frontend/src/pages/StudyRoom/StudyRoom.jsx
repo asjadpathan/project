bf76d66bf7a8865356dc3bb11930f58080e869a3
Swapnil Pawar <swapnildpawar011@gmail.com>
2026-07-31 20:07:39 +0530

    Create other files structure for diffrent pages

---
+++ frontend/src/pages/StudyRoom/StudyRoom.jsx
@@ -0,0 +1,20 @@
+﻿import './StudyRoom.css'
+import PomodoroTimer from './components/PomodoroTimer'
+import AmbientAudioPlayer from './components/AmbientAudioPlayer'
+import ProductivityAnalytics from './components/ProductivityAnalytics'
+
+const StudyRoom = () => {
+  return (
+    <main className="">
+      <div className="__container">
+        <h1>Study Room</h1>
+        <PomodoroTimer />
+        <AmbientAudioPlayer />
+        <ProductivityAnalytics />
+      </div>
+    </main>
+  )
+}
+
+export default StudyRoom
+
--- frontend/src/pages/landingpage/landingpage.jsx
+++ frontend/src/pages/landingpage/landingpage.jsx
@@ -1,13 +1,13 @@
-import HeroSection from './components/herosection'
-import FeaturesSection from './components/featuressection'
-import StatSection from './components/statsection'
-import './landingpage.css'
+import HeroSection from './components/HeroSection'
+import FeaturesSection from './components/FeaturesSection'
+import StatsSection from './components/StatsSection'
+import './LandingPage.css'
 
 const LandingPage = () => {
   return (
     <main className="landing-page">
       <HeroSection />
-      <StatSection />
+      <StatsSection />
       <FeaturesSection />
     </main>
   )
